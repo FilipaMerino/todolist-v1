1f0495cf6b259a104ea791f67d15dae77131f8e1
Filipa Merino <filipaam@outlook.com>
2023-03-17 12:36:14 +0000

    ejs variables

--- app.js
+++ app.js
@@ -5,40 +5,29 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // ejs alwas has to come after initialized app
-app.use("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 
 
 
 
-app.get("/", function(req,res){
+app.get("/", function(req, res){
 
-  res.render("index");
+
 
   var today = new Date ();
   var currentDay = today.getDay();
-  var numberOfDays = 6 - today.getDay();
+  var day = "";
 
-
-
-
-
-  if(currentDay === 6 || currentDay === 6){
-    res.send("<h1>YAY! It's the weekend!</h1>");
-  } else {
-    if(numberOfDays === 1){
-      res.sendFile(__dirname + "/index.html")
-    } else{
-    // res.send("<h1>Boo I have to work!</h1>" + "<p>There's " + numberOfDays + " days for the weekend </p>");
-      res.sendFile(__dirname + "/index.html")
+  if(currentDay === 0 || currentDay === 6){
+      day = "Weekend";
+    } else {
+      day = "Weekday";
     }
-  }
 
+    res.render("list", {kindOfDay: day});
 
-
-});
-
-
+  });
 
 
 
