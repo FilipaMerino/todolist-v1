f9ba9ad443c19dd9ff6143b6cba70cd7e556ae90
Filipa Merino <filipaam@outlook.com>
2023-03-17 14:25:48 +0000

    creating new tasks

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+var items = []; // this is necessary in order to be able to use a variable from a post request to a get request.
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,16 +22,19 @@ app.get("/", function(req, res){
   };
 
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day});
+
+  res.render("list", {kindOfDay: day, newListItem: items});
 
 });
 
 
 app.post("/", function(req, res){
 
-  var taskList = req.body.newItem;
-  console.log(taskList);
+  var item = req.body.newItem;
+  items.push(item);
 
+  res.redirect("/"); //When a post request is triggered in our home route, we'll save the value of new item inside that text box;
+  // store it in a variable called item, and redirect to the home route which then
 
 });
 
