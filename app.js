ed3eb0095bea4beea9831c5017d41b780ce6d00d
Filipa Merino <filipaam@outlook.com>
2023-03-17 12:17:44 +0000

    ejs packages installed and set

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// ejs alwas has to come after initialized app
+app.use("view engine", "ejs");
+
+
 
 
 
 app.get("/", function(req,res){
-  res.send("<h1>YOUR'RE IN!</h1>");
+
+  res.render("index");
+
+  var today = new Date ();
+  var currentDay = today.getDay();
+  var numberOfDays = 6 - today.getDay();
+
+
+
+
+
+  if(currentDay === 6 || currentDay === 6){
+    res.send("<h1>YAY! It's the weekend!</h1>");
+  } else {
+    if(numberOfDays === 1){
+      res.sendFile(__dirname + "/index.html")
+    } else{
+    // res.send("<h1>Boo I have to work!</h1>" + "<p>There's " + numberOfDays + " days for the weekend </p>");
+      res.sendFile(__dirname + "/index.html")
+    }
+  }
+
+
+
 });
+
+
+
 
 
 
